07da4501a9b92c9b8051ee0a26825e9ee2968ec6
PamelaPz <jpamelapz02@gmail.com>
2021-02-04 12:09:15 -0600

    crear tabla para contacto

---
+++ database/migrations/2021_02_04_174041_create_contact_table.php
@@ -0,0 +1,40 @@
+conta<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+
+            $table->text('address');
+            $table->string('telephone');
+            $table->string('telephone_office');
+            $table->string('email_admin');
+            $table->string('email_sales');
+            $table->string('schedule');
+            $table->text('url')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
