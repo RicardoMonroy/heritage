784dc690a6106f0417445355e39705eb905b7d3e
PamelaPz <jpamelapz02@gmail.com>
2021-02-05 12:33:10 -0600

    Obtención de datos por web scraping

--- app/Http/Controllers/ScrapingController.php
+++ app/Http/Controllers/ScrapingController.php
@@ -8,19 +8,27 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class ScrapingController extends Controller
 {
-    public function example(Client $client) 
+    public function index(Client $client) 
     {
         $crawler = $client->request('GET', 'https://www.elfinanciero.com.mx/mis-finanzas');
         $classFilter = 'columns is-mobile item';
-        $contact = $crawler->filter("[class='$classFilter']")->each( function (Crawler $contactNode) {
-            // $nameNode = $contactNode->filter("[class='head']")->first();
-            // echo $nameNode->text() . '</br>';
-            $divs = $contactNode->children()->filter('div');
+
+        $new = [];
+
+        $crawler->filter("[class='$classFilter']")->each( function (Crawler $newsFinanzas) use (&$news) {
+            $new = [];
+
+            $divs = $newsFinanzas->children()->filter('div');
             $sectionImg = $divs->eq(0); //No se pueden obtener imgs
             $sectionInfo = $divs->eq(1);
-                $date = $sectionInfo->filter("[class='date-time is-hidden-mobile']")->text();
-                $title = $sectionInfo->filter("[class='head']")->text();
+
+            $new['date'] = $sectionInfo->filter("[class='date-time is-hidden-mobile']")->text();
+            $new['title'] = $sectionInfo->filter("[class='head']")->text();
+
+            $news[] = $new;
         });
-        dd($contact->text()); 
+
+        dd($news);
+        // return view('blog', compact('news'));
     }
 }
