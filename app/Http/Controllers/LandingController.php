f20eef81e33a027be55f6921e3c1a78262537458
PamelaPz <jpamelapz02@gmail.com>
2021-02-12 11:17:42 -0600

    aplicando web scraping en controlador blog

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\About;
 use App\Company;
 use App\Slider;
+use Goutte\Client;
 use Illuminate\Http\Request;
+use Symfony\Component\DomCrawler\Crawler;
 
 class LandingController extends Controller
 {
@@ -18,8 +20,23 @@ class LandingController extends Controller
         return view('welcome', compact('slider', 'about', 'companies'));
     }
 
-    public function blog()
+    public function blog(Client $client)
     {
-        return view('blog');
+        $crawlerF = $client->request('GET', 'https://www.elfinanciero.com.mx/nacional');
+
+        $new = [];
+
+        $crawlerF->filter('.column .feed a')->each( function (Crawler $newsAll) use (&$newsF) {
+            $new = [];
+            
+            $new['url']   = $newsAll->filter('a')->first()->attr('href');
+            $new['date']  = $newsAll->filter('p.date-time.is-hidden-mobile')->text();
+            $new['title'] = $newsAll->filter('p.head')->text();
+
+            $newsF[] = $new;
+        });
+
+        // dd($news);
+        return view('blog', compact('newsF'));
     }
 }
